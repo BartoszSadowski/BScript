e27ef2e911186fbee678264601d6e0459edc4b36
Sadaffi <bartek_sadowski@o2.pl>
2021-03-27 22:01:24 +0100

    [basic-flow] Removed function for entering start

--- src/logic/listener.js
+++ src/logic/listener.js
@@ -1,10 +1,6 @@
 import BScriptListener from '../grammar/antlrResult/BScriptListener.js';
 
 export default class Listener extends BScriptListener {
-    enterStart(ctx) {
-        console.log("Begin start");
-    }
-
     exitStart(ctx) {
         console.log("Exit start");
     }
